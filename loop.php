89c20fac94ffe96e8cc5dfa60d76ef3d1df86cf5
Evan Fields <44267068+freindlyneighborspidey999@users.noreply.github.com>
2019-03-08 09:05:19 -0500

    Add files via upload

---
+++ loop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($x = 0; $x <= 15; $x++) {
+  echo "$x <br>";
+}
+?>  
+<?php 
+//$colors = array("red", "green", "blue", "yellow"); 
+
+//foreach ($colors as $value) {
+    //echo "$value <br>";
+//}
+?>
+</body>
+</html>
